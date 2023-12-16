34b744713eac1e8952ff26f8cdd20d2d76f5ed3a
stefan-ty-su <stefansu3526@gmail.com>
2023-12-16 14:21:44 +1100

    Made isTerminal a general static function

--- connectFourGame.py
+++ connectFourGame.py
@@ -1,16 +1,16 @@
 
 class C4Board:
 
+    height = 6 # Col -> Transposed Row
+    length = 7 # Row -> Transposed Col
     def __init__(self) -> None:
         
-        self.height = 6 # Col -> Transposed Row
-        self.length = 7 # Row -> Transposed Col
-        self.board = [['' for i in range(self.height)] for j in range(self.length)]
+        self.board = [['' for i in range(C4Board.height)] for j in range(C4Board.length)]
         self.turn = 0
 
     def playMove(self, row: int) -> None:
 
-        if row >= self.length or row < 0:
+        if row >= C4Board.length or row < 0:
             raise ValueError(f'Row Value: {row} is invalid')
 
         # Assigning char for player ('x' always goes first)
@@ -21,74 +21,74 @@ class C4Board:
             player = 'o'
             self.turn = 0
 
-        for j in range(self.height): # Iterating for through each value in row
+        for j in range(C4Board.height): # Iterating for through each value in row
             num = self.board[row][j]
             if num == '':
                 self.board[row][j] = player
                 break
         else: # If loop runs and break does not occur, then play is invalid
             raise ValueError(f'Row {row} is full')
-    
+
     def getValidMoves(self) -> list[int]:
 
         validMoves = []
-        for row in range(self.length):
-            for i in range(self.height-1, -1, -1):
+        for row in range(C4Board.length):
+            for i in range(C4Board.height-1, -1, -1):
                 if self.board[row][i] == '':
                     validMoves.append(row)
                     break
         
         return validMoves
-    
-    def isTerminal(self) -> bool:
-
-        # Checking Rows
-        for row in self.board:
-            for j in range(self.height-3):
-                if row[j] != '' and row[j] == row[j+1] == row[j+2] == row[j+3]:
-                    print('A')
-                    return True
-        
-        # Checking Cols
-        for j in range(self.height):
-            for i in range(self.length-3):
-                if self.board[i][j] != '' and self.board[i][j] == self.board[i+1][j] == self.board[i+2][j] == self.board[i+3][j]:
-                    return True
-        
-        # Checking Descending Diagonals
-        for i in range(self.length-3):
-            for j in range(self.height-3):
-                if self.board[i][j] != '' and self.board[i][j] == self.board[i+1][j+1] == self.board[i+2][j+2] == self.board[i+3][j+3]:
-                    return True
-        
-        # Checking Ascending Diagonals
-        for i in range(self.length-3):
-            for j in range(self.height-1, 3, -1):
-                if self.board[i][j] != '' and self.board[i][j] == self.board[i+1][j-1] == self.board[i+2][j-2] == self.board[i+3][j-3]:
-                    return True
-        
-        filled = True
-        for i in range(self.length):
-            if self.board[i][self.height-1] == '':
-                filled = False
-
-        return filled
 
     def getTurn(self):
         return self.turn
 
+def isTerminal(board: list[list[str]]) -> bool:
+
+    # Checking Rows
+    for row in board:
+        for j in range(C4Board.height-3):
+            if row[j] != '' and row[j] == row[j+1] == row[j+2] == row[j+3]:
+                print('A')
+                return True
+    
+    # Checking Cols
+    for j in range(C4Board.height):
+        for i in range(C4Board.length-3):
+            if board[i][j] != '' and board[i][j] == board[i+1][j] == board[i+2][j] == board[i+3][j]:
+                return True
+    
+    # Checking Descending Diagonals
+    for i in range(C4Board.length-3):
+        for j in range(C4Board.height-3):
+            if board[i][j] != '' and board[i][j] == board[i+1][j+1] == board[i+2][j+2] == board[i+3][j+3]:
+                return True
+    
+    # Checking Ascending Diagonals
+    for i in range(C4Board.length-3):
+        for j in range(C4Board.height-1, 3, -1):
+            if board[i][j] != '' and board[i][j] == board[i+1][j-1] == board[i+2][j-2] == board[i+3][j-3]:
+                return True
+    
+    filled = True
+    for i in range(C4Board.length):
+        if board[i][C4Board.height-1] == '':
+            filled = False
+
+    return filled
 
 
-board = C4Board()
-board.board = [
-    ['x', 'o', 'x', 'o', 'x', 'o'],
-    ['', '', '', 'o', '', ''],
-    ['', '', '', 'o', '', ''],
-    ['', '', 'o', 'o', '', ''],
-    ['', '', '', '', '', ''],
-    ['', '', '', '', '', ''],
-    ['', '', '', '', '', '']
-]
-print(board.isTerminal())
+if __name__ == "__main__":
+    board = C4Board()
+    board.board = [
+        ['x', 'o', 'x', 'o', 'x', 'o'],
+        ['', '', '', 'o', '', ''],
+        ['', '', '', 'o', '', ''],
+        ['', '', 'o', 'o', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', '']
+    ]
+    print(isTerminal(board.board))
 
         
