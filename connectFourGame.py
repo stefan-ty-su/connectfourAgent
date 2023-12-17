7eba49662a5a17fa87c237bc2f3b6d5f4f6b58c2
stefan-ty-su <stefansu3526@gmail.com>
2023-12-17 17:13:20 +1100

    Added playability with board and user interaction

--- connectFourGame.py
+++ connectFourGame.py
@@ -5,7 +5,7 @@ class C4Board:
     length = 7 # Row -> Transposed Col
     def __init__(self) -> None:
         
-        self.board = [['' for i in range(C4Board.height)] for j in range(C4Board.length)]
+        self.board = [[' ' for i in range(C4Board.height)] for j in range(C4Board.length)]
         self.turn = 0
 
     def playMove(self, row: int) -> None:
@@ -13,6 +13,7 @@ class C4Board:
         if row >= C4Board.length or row < 0:
             raise ValueError(f'Row Value: {row} is invalid')
 
+        print(f'Playing move at column: {row}')
         # Assigning char for player ('x' always goes first)
         if self.turn == 0:
             player = 'x'
@@ -23,7 +24,7 @@ class C4Board:
 
         for j in range(C4Board.height): # Iterating for through each value in row
             num = self.board[row][j]
-            if num == '':
+            if num == ' ':
                 self.board[row][j] = player
                 break
         else: # If loop runs and break does not occur, then play is invalid
@@ -34,7 +35,7 @@ class C4Board:
         validMoves = []
         for row in range(C4Board.length):
             for i in range(C4Board.height-1, -1, -1):
-                if self.board[row][i] == '':
+                if self.board[row][i] == ' ':
                     validMoves.append(row)
                     break
         
@@ -48,46 +49,54 @@ def boardIsTerminal(board: list[list[str]]) -> bool:
     # Checking Rows
     for row in board:
         for j in range(C4Board.height-3):
-            if row[j] != '' and row[j] == row[j+1] == row[j+2] == row[j+3]:
+            if row[j] != ' ' and row[j] == row[j+1] == row[j+2] == row[j+3]:
                 return True
     
     # Checking Cols
     for j in range(C4Board.height):
         for i in range(C4Board.length-3):
-            if board[i][j] != '' and board[i][j] == board[i+1][j] == board[i+2][j] == board[i+3][j]:
+            if board[i][j] != ' ' and board[i][j] == board[i+1][j] == board[i+2][j] == board[i+3][j]:
                 return True
     
     # Checking Descending Diagonals
     for i in range(C4Board.length-3):
         for j in range(C4Board.height-3):
-            if board[i][j] != '' and board[i][j] == board[i+1][j+1] == board[i+2][j+2] == board[i+3][j+3]:
+            if board[i][j] != ' ' and board[i][j] == board[i+1][j+1] == board[i+2][j+2] == board[i+3][j+3]:
                 return True
     
     # Checking Ascending Diagonals
     for i in range(C4Board.length-3):
         for j in range(C4Board.height-1, 3, -1):
-            if board[i][j] != '' and board[i][j] == board[i+1][j-1] == board[i+2][j-2] == board[i+3][j-3]:
+            if board[i][j] != ' ' and board[i][j] == board[i+1][j-1] == board[i+2][j-2] == board[i+3][j-3]:
                 return True
     
     filled = True
     for i in range(C4Board.length):
-        if board[i][C4Board.height-1] == '':
+        if board[i][C4Board.height-1] == ' ':
             filled = False
 
     return filled
 
+def printBoard(board: list[list[str]]) -> bool:
+
+    rotatedBoard = [[board[i][j] for i in range(len(board))] for j in range(len(board[0])-1, -1, -1)]
+    print("\n")
+    for col in rotatedBoard:
+        print(col)
+
 
 if __name__ == "__main__":
     board = C4Board()
+    board.turn = 1
     board.board = [
-        ['x', 'o', 'x', 'o', 'x', 'o'],
-        ['', '', '', '', '', ''],
-        ['', '', '', '', '', ''],
-        ['', '', '', '', '', ''],
-        ['', '', '', '', '', ''],
-        ['', '', '', '', '', ''],
-        ['', '', '', '', '', '']
+        ['x', 'x', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' '],
+        ['o', 'o', 'x', 'o', ' ', ' '],
+        ['x', 'x', 'o', 'o', ' ', ' '],
+        ['x', 'x', 'x', 'o', ' ', ' '],
+        ['x', 'o', ' ', ' ', ' ', ' '],
+        ['o', 'o', ' ', ' ', ' ', ' ']
     ]
-    print(boardIsTerminal(board.board))
+    print(board.evaluate())
 
         
